dd7486153195357840dd6b0d9490b50ea39a536a
angelo.eap <angelo.eap@epita.fr>
2024-06-30 05:53:00 +0200

    [aep/hdfs] feat: first stats with file put in hdfs by hand

--- analysis/src/main/scala/Main.scala
+++ analysis/src/main/scala/Main.scala
@@ -1,21 +1,32 @@
-import org.apache.spark.sql.{SaveMode, SparkSession}
+import org.apache.spark.sql.{DataFrame, SparkSession}
 import org.apache.spark.sql.functions._
 
 object Main {
+  val hdfs_master = "hdfs://localhost:9000/"
+  val spark = SparkSession.builder
+    .appName("Analysis")
+    .master("local")
+    .getOrCreate()
+
+  spark.sparkContext.setLogLevel("ERROR")
+  import spark.implicits._
+
   def main(args: Array[String]): Unit = {
-    val spark = SparkSession.builder
-      .appName("Analysis")
-      .master("local")
-      .getOrCreate()
 
-    spark.sparkContext.setLogLevel("ERROR")
-    import spark.implicits._
 
-    val hdfs_master = "hdfs://localhost:9000/"
+    val df = spark.read.option("inferSchema", "true").option("multiline", "true").format("json").load(hdfs_master + "data/openbeer/breweries/test.json")
+    df.show()
 
-    val df_csv = spark.read.option("inferSchema", "true").option("header", "true").csv(hdfs_master + "data/openbeer/breweries/breweries.csv")
-    df_csv.show()
-
+    avgStatsOfDrone(df, 1)
     spark.stop()
+  }
+
+  def avgStatsOfDrone(data: DataFrame, droneId: Int): Unit = {
+    val filteredData = data.filter($"id" === droneId)
+    val avgTemperature = filteredData.agg(avg("temperature")).as[Double].first()
+    val avgBattery = filteredData.agg(avg("battery")).as[Double].first()
+
+    println(s"The average temperature of drone $droneId is: $avgTemperature")
+    println(s"The average battery level of drone $droneId is: $avgBattery")
   }
 }
