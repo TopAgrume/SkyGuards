36f4581e8ba6bd6b543e74d1b25ff010dc1bc044
angelo.eap <angelo.eap@epita.fr>
2024-06-30 20:45:30 +0200

    [main]: add more stats and read from hdfs

--- analysis/src/main/scala/Main.scala
+++ analysis/src/main/scala/Main.scala
@@ -1,5 +1,18 @@
 import org.apache.spark.sql.{DataFrame, SparkSession}
 import org.apache.spark.sql.functions._
+import org.apache.spark.sql.Encoders
+
+final case class Location(lon: Double, lat: Double)
+final case class Report(
+                         id: Int,
+                         timestamp: String,
+                         pos: Location,
+                         nbPeople: Int,
+                         surface: Int,
+                         speed: Int,
+                         battery: Int,
+                         temperature: Int
+                       )
 
 object Main {
   val hdfs_master = "hdfs://localhost:9000/"
@@ -12,21 +25,39 @@ object Main {
   import spark.implicits._
 
   def main(args: Array[String]): Unit = {
+    val schema = Encoders.product[Report].schema
 
-
-    val df = spark.read.option("inferSchema", "true").option("multiline", "true").format("json").load(hdfs_master + "data/openbeer/breweries/test.json")
+    val df = spark.read.schema(schema).json(hdfs_master + "user/hdfs/reports/*.json")
     df.show()
 
-    avgStatsOfDrone(df, 1)
+    df.select("id").distinct().orderBy("id").collect().foreach { row =>
+      val droneId = row.getAs[Int]("id")
+      println(s"Statistics for drone $droneId:")
+      calculateStats(df, droneId)
+    }
+
     spark.stop()
   }
 
-  def avgStatsOfDrone(data: DataFrame, droneId: Int): Unit = {
+  def calculateStats(data: DataFrame, droneId: Int): Unit = {
     val filteredData = data.filter($"id" === droneId)
-    val avgTemperature = filteredData.agg(avg("temperature")).as[Double].first()
-    val avgBattery = filteredData.agg(avg("battery")).as[Double].first()
 
-    println(s"The average temperature of drone $droneId is: $avgTemperature")
-    println(s"The average battery level of drone $droneId is: $avgBattery")
+    val peoplePerM2 = filteredData.select(avg($"nbPeople" / $"surface")).as[Double].first()
+    val maxTemperature = filteredData.agg(max("temperature")).as[Int].first()
+    val avgTemperature = filteredData.agg(avg("temperature")).as[Double].first()
+    val lastBattery = filteredData.orderBy(desc("timestamp")).select("battery").as[Int].first()
+    val avgSpeed = filteredData.agg(avg("speed")).as[Double].first()
+    val firstPos = filteredData.orderBy(asc("timestamp")).select("pos.*").as[Location].first()
+    val lastPos = filteredData.orderBy(desc("timestamp")).select("pos.*").as[Location].first()
+
+    // Display statistics
+    println(f"Number of people per m2: $peoplePerM2%.2f")
+    println(s"Maximum temperature: $maxTemperature")
+    println(s"Average temperature: $avgTemperature")
+    println(s"Last battery level: $lastBattery")
+    println(s"Average speed: $avgSpeed")
+    println(s"First position: ${firstPos.lon}, ${firstPos.lat}")
+    println(s"Last position: ${lastPos.lon}, ${lastPos.lat}")
+    println()
   }
 }
--- hdfs/src/main/scala/Main.scala
+++ hdfs/src/main/scala/Main.scala
@@ -1,5 +1,6 @@
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types._
 
 object Main {
 
@@ -10,6 +11,8 @@ object Main {
       .master("local")
       .getOrCreate()
 
+    import spark.implicits._
+
     val kafkaDF = spark.read
       .format("kafka")
       .option("kafka.bootstrap.servers", "localhost:29092")
@@ -17,7 +20,23 @@ object Main {
       .option("startingOffsets", "earliest")
       .load()
 
-    val messagesDF = kafkaDF.selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)")
+    // Define the schema of your JSON data
+    val jsonSchema = new StructType()
+    .add("id", IntegerType)
+    .add("timestamp", StringType)
+    .add("pos", new StructType()
+      .add("lon", DoubleType)
+      .add("lat", DoubleType))
+    .add("nbPeople", IntegerType)
+    .add("surface", IntegerType)
+    .add("speed", IntegerType)
+    .add("battery", IntegerType)
+    .add("temperature", IntegerType)
+
+    val messagesDF = kafkaDF
+      .selectExpr("CAST(value AS STRING) as json")
+      .select(from_json(col("json"), jsonSchema).as("data"))
+      .select("data.*")
 
     messagesDF.printSchema()
     println(messagesDF.show())
@@ -28,5 +47,7 @@ object Main {
     messagesDF.write
       .mode("append")
       .json(hdfsPath)
+
+    spark.stop()
   }
 }
